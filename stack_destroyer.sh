ebf7c9d5348c591f3093c2642f213e0fa9037fab
3king <tre@icandylabs.com>
2016-08-29 18:57:13 -0500

    new syntax

--- stack_destroyer.sh
+++ stack_destroyer.sh
@@ -2,10 +2,10 @@
 
 set v
 
-for stacks in $(aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --query "StackSummaries[].StackId" | grep "Jenkins-[A-Z]*-[0-9]*-[0-9]*"  | sed s/\"//g | sed s/\,//g); do 
+for stacks in $(aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --query "StackSummaries[].StackName" | grep "Jenkins-[A-Z]*-[0-9]*-[0-9]*"  | sed s/\"//g | sed s/\,//g | sed s/\ //g); do 
   time=$(echo $stacks | cut -d'/' -f2 | cut -d'-' -f4)
   thirty_minutes_ago=$(date -d "30 min ago" +%s)
   if [ "$time" -lt "$thirty_minutes_ago" ]; then 
-    aws cloudformation delete-stack --stackId ${stacks}
+    aws cloudformation delete-stack --stack-name ${stacks}
   fi;
 done
